a976ce25a81d35b052ae28e556174db14f3b82ae
trongquy269 <quyb2017000@student.ctu.edu.vn>
2024-11-01 18:20:07 +0700

    Hien thi danh sach cac contact lay ve tu server

--- vite.config.js
+++ vite.config.js
@@ -15,5 +15,15 @@ export default defineConfig({
 	},
 	server: {
 		port: 3001,
+		proxy: {
+			'/api': {
+				target: 'http://localhost:3000',
+				changeOrigin: true,
+			},
+		},
+	},
+	optimizeDeps: {
+		include: [],
+		exclude: [],
 	},
 });
